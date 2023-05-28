1edcd25ca022a933d417a0eece688452ae6b2699
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-05-28 16:04:42 -0300

    Adição do entity framework na aplicação

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IQueryable<Evento> Get()
+        {
+            return _context.Set<Evento>().AsQueryable();
+        }
+
+        [HttpGet("{id}")]
+        public Evento Get(int id)
+        {
+            return _context.Set<Evento>().FirstOrDefault(e => e.EventoId == id);
+        }
+    }
+}
