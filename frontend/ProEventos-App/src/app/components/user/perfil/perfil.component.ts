8599fb2b565dde8eb0a3ef7d62a7efb8d1842214
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-08-14 20:26:21 -0300

    Adição de funcionalidades na tela de perfil

--- frontend/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ frontend/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ValidatorField } from '@app/helpers/ValidatorField';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  id!: string;
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    ) {}
+
+    ngOnInit(): void {
+      this.id = this.route.snapshot.params.id;
+
+      const formOptions: AbstractControlOptions = {
+        validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+      };
+
+      this.form = this.fb.group({
+        titulo: ['', Validators.required],
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        telefone: ['', [Validators.required]],
+        descricao: ['', Validators.required],
+        funcao: ['', Validators.required],
+        senha: ['', [Validators.minLength(6), Validators.nullValidator]],
+        confirmeSenha: ['', Validators.nullValidator]
+      }, formOptions);
+    }
+
+    // Conveniente para pegar um FormField apenas com a letra F
+    get f(): any { return this.form.controls; }
+
+    public onSubmit(): void {
+      if(this.form.invalid) return;
+
+    }
+
+    public resetForm(event: any): void {
+      event.preventDefault();
+      this.form.reset();
+    }
 
 }
