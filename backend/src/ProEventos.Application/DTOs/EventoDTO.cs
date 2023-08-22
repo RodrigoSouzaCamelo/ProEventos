589aafbedd4ecfb2be659cc0b231802e9f2ccca9
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-08-22 18:44:20 -0300

    Adição de annotations de validação

--- backend/src/ProEventos.Application/DTOs/EventoDTO.cs
+++ backend/src/ProEventos.Application/DTOs/EventoDTO.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProEventos.Application.DTOs
 {
@@ -7,10 +9,18 @@ namespace ProEventos.Application.DTOs
         public int Id { get; set; }
         public string Local { get; set; }
         public string Data { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório."),
+         MinLength(4, ErrorMessage = "{0} deve ter no mínimo 4 caracteres."),
+         MaxLength(50, ErrorMessage = "{0} deve ter no máximo 50 caracteres.")]
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} precisa ser um e-mail válido.")]
         public string Email { get; set; }
         public IEnumerable<LoteDTO> Lotes { get; set; }
         public IEnumerable<RedeSocialDTO> RedesSociais { get; set; }
