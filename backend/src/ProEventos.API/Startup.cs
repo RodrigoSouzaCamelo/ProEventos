49d44ec434f14c7756d6b21dd929d00cbeab4d4e
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-09-13 19:50:59 -0300

    Injeção de dependências

--- backend/src/ProEventos.API/Startup.cs
+++ backend/src/ProEventos.API/Startup.cs
@@ -26,8 +26,12 @@ namespace ProEventos.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ILoteService, LoteService>();
             services.AddScoped<IEventoService, EventoService>();
+
+            services.AddScoped<ILoteRepository, LoteRepository>();
             services.AddScoped<IEventoRepository, EventoRepository>();
+
             services.AddDbContext<ProEventosContext>(
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
