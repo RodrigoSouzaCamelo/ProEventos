dbf9956aad96e3e1d287fa77ec083de2ee7f8601
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-10-10 19:52:38 -0300

    Criação do serviço de token

---
+++ backend/src/ProEventos.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using ProEventos.Application.DTOs;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> CreateToken(UserUpdateDTO userUpdateDTO);
+    }
+}
---
+++ backend/src/ProEventos.Application/Services/TokenService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProEventos.Application.DTOs;
+using ProEventos.Application.Interfaces;
+using ProEventos.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly SymmetricSecurityKey _symmetricSecurityKey;
+
+        public TokenService(IMapper mapper, 
+            IConfiguration configuration, 
+            UserManager<User> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+
+        public async Task<string> CreateToken(UserUpdateDTO userUpdateDTO)
+        {
+            var user = _mapper.Map<User>(userUpdateDTO);
+
+            var claims = new List<Claim>() 
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+            
+            var signingCredentials = new SigningCredentials(_symmetricSecurityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = signingCredentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
