17ddd8be6cb8dfed200aa7c5e522e12f2913d38a
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-10-18 21:02:03 -0300

    Refatoração de código e adição de token no serviço de eventos

--- frontend/ProEventos-App/src/app/services/evento.service.ts
+++ frontend/ProEventos-App/src/app/services/evento.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
@@ -10,11 +10,13 @@ import { environment } from '@environments/environment';
 )
 export class EventoService {
   private baseURL = environment.apiUrl + '/api/evento';
+  private tokenHeader = new HttpHeaders({ 'Authorization': 'Bearer '});
+
   constructor(private http: HttpClient) { }
 
   public getEventos(): Observable<Evento[]> {
     return this.http
-      .get<Evento[]>(`${this.baseURL}`)
+      .get<Evento[]>(`${this.baseURL}`, { headers: this.tokenHeader })
       .pipe(take(3));
   }
 
@@ -26,25 +28,25 @@ export class EventoService {
 
   public getEventosByTema(tema: string): Observable<Evento[]> {
     return this.http
-      .get<Evento[]>(`${this.baseURL}/tema/${tema}`)
+      .get<Evento[]>(`${this.baseURL}/tema/${tema}`, { headers: this.tokenHeader })
       .pipe(take(3));
   }
 
   public post(evento: Evento): Observable<Evento> {
     return this.http
-      .post<Evento>(`${this.baseURL}`, evento)
+      .post<Evento>(`${this.baseURL}`, evento, { headers: this.tokenHeader })
       .pipe(take(3));
   }
 
   public put(evento: Evento): Observable<Evento> {
     return this.http
-      .put<Evento>(`${this.baseURL}`, evento)
+      .put<Evento>(`${this.baseURL}`, evento, { headers: this.tokenHeader })
       .pipe(take(3));
   }
 
   public delete(id: number): Observable<Evento> {
     return this.http
-      .delete<Evento>(`${this.baseURL}/${id}`)
+      .delete<Evento>(`${this.baseURL}/${id}`, { headers: this.tokenHeader })
       .pipe(take(3));
   }
 
@@ -54,7 +56,7 @@ export class EventoService {
     formData.append('file', fileToUpload)
 
     return this.http
-      .post<Evento>(`${this.baseURL}/upload/image/${eventoId}`, formData)
+      .post<Evento>(`${this.baseURL}/upload/image/${eventoId}`, formData, { headers: this.tokenHeader })
       .pipe(take(3))
   }
 }
