d7c1465c6686c1ba36a3a9e51a27fbbbffe1103b
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-10-30 19:36:38 -0300

    Criação da funcionalidade de atualizar perfil no frontend

--- frontend/ProEventos-App/src/app/services/account.service.ts
+++ frontend/ProEventos-App/src/app/services/account.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '@app/models/User';
+import { UserUpdate } from '@app/models/UserUpdate';
 import { environment } from '@environments/environment';
 import { Observable, ReplaySubject } from 'rxjs';
 import { map, take } from 'rxjs/operators';
@@ -18,12 +19,12 @@ export class AccountService {
 
   public login(model: any): Observable<void> {
     return this.http.post<User>(`${this.baseURL}/login`, model)
-      .pipe(
+    .pipe(
         take(3),
         map(user => {
           if(user) this.setCurrentUser(user);
         })
-      );
+    );
   }
 
   public register(model: any): Observable<void> {
@@ -41,8 +42,24 @@ export class AccountService {
     this.currentUserSource.next(undefined);
   }
 
+  public getUser(): Observable<UserUpdate> {
+    return this.http.get<UserUpdate>(`${this.baseURL}/user`)
+      .pipe(take(3));
+  }
+
+  public updateUser(model: UserUpdate): Observable<void> {
+    return this.http.put<UserUpdate>(`${this.baseURL}/user`, model)
+      .pipe(
+        take(3),
+        map((user: UserUpdate) => {
+          this.setCurrentUser(user);
+        })
+      )
+  }
+
   private setCurrentUser(user: User): void {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
+
 }
