5f89f28af57a7b4a3e5fd50510f7766aca93ebc0
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-11-09 18:57:06 -0300

    Criação do repositório de rede social

---
+++ backend/src/ProEventos.Domain/Interfaces/Repositories/IRedeSocialRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProEventos.Domain.Models;
+using System.Threading.Tasks;
+
+namespace ProEventos.Domain.Interfaces.Repositories
+{
+    public interface IRedeSocialRepository : IRepository<RedeSocial>
+    {
+        Task<RedeSocial[]> GetAllRedeSocialByEventoAsync(int eventoId);
+        Task<RedeSocial> GetRedeSocialByEventoAsync(int eventoId, int id);
+        Task<RedeSocial[]> GetAllRedeSocialByPalestranteAsync(int palestranteId);
+        Task<RedeSocial> GetRedeSocialByPalestranteAsync(int palestranteId, int id);
+    }
+}
---
+++ backend/src/ProEventos.Persistence/Repositories/RedeSocialRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProEventos.Domain.Interfaces.Repositories;
+using ProEventos.Domain.Models;
+using ProEventos.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence.Repositories
+{
+    internal class RedeSocialRepository : Repository<RedeSocial>, IRedeSocialRepository
+    {
+        protected RedeSocialRepository(ProEventosContext context) : base(context)
+        {
+        }
+
+        public async Task<List<RedeSocial>> GetAllRedeSocialByEventoAsync(int eventoId)
+        {
+            return await _context
+                .Set<RedeSocial>()
+                .AsNoTracking()
+                .Where(rs => rs.EventoId == eventoId)
+                .ToListAsync();
+        }
+
+        public async Task<List<RedeSocial>> GetAllRedeSocialByPalestranteAsync(int palestranteId)
+        {
+            return await _context
+                .Set<RedeSocial>()
+                .AsNoTracking()
+                .Where(rs => rs.PalestranteId == palestranteId)
+                .ToListAsync();
+        }
+
+        public async Task<RedeSocial> GetRedeSocialByEventoAsync(int eventoId, int id)
+        {
+            return await _context
+                .Set<RedeSocial>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(rs => rs.EventoId == eventoId && rs.Id == id);
+        }
+
+        public async Task<RedeSocial> GetRedeSocialByPalestranteAsync(int palestranteId, int id)
+        {
+            return await _context
+                .Set<RedeSocial>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(rs => rs.PalestranteId == palestranteId && rs.Id == id);
+        }
+    }
+}
