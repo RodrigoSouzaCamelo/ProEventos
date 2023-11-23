3550e1e17ecc30a2de96de0bb36c5767e16d3c77
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-11-23 18:56:18 -0300

    Criação da funcionalidade de upload de imagem na tela de perfil

--- frontend/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ frontend/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControlOptions,
-  FormBuilder,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ValidatorField } from '@app/helpers/ValidatorField';
 import { UserUpdate } from '@app/models/UserUpdate';
 import { AccountService } from '@app/services/account.service';
+import { environment } from '@environments/environment';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 
@@ -19,16 +12,47 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class PerfilComponent implements OnInit {
   userUpdate = {} as UserUpdate;
+  public imagemURL = 'assets/img/perfil.png';
+  public file: any | File;
 
   public get ehPalestrante(): boolean {
     return this.userUpdate.funcao === "Palestrante";
   }
 
-  constructor() {}
+  constructor(private spinner: NgxSpinnerService,
+              private toastr: ToastrService,
+              private accountService: AccountService) {}
 
   ngOnInit(): void {}
 
   public setChangeUserForm(user: UserUpdate) {
+    if(user.imagemURL) {
+      this.imagemURL = environment.apiUrl  + '/resources/perfil/' + user.imagemURL;
+    }
+
     this.userUpdate = { ...user };
+  }
+
+  public onFileChange(event: any): void {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => this.imagemURL = e.target.result;
+
+    this.file = event.target.files;
+    reader.readAsDataURL(this.file[0]);
+
+    this.uploadImage();
+  }
+
+  private uploadImage(): void {
+    this.spinner.show();
+    this.accountService.postUpload(this.file)
+    .subscribe(
+      () => this.toastr.success('Imagem atualizada com sucesso.', 'Sucesso'),
+      (error: any) =>{
+        console.error(error);
+        this.toastr.error('Não foi possível fazer upload de imagem', 'Erro');
+      }
+    ).add(() => this.spinner.hide())
   }
 }
