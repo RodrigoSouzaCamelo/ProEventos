16e52b46f5a5794188548c9f64af0a9f4f19d8f4
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-11-30 19:03:04 -0300

    Criação da edição do palestrante no frontend

--- frontend/ProEventos-App/src/app/components/palestrantes/palestrante-detalhe/palestrante-detalhe.component.ts
+++ frontend/ProEventos-App/src/app/components/palestrantes/palestrante-detalhe/palestrante-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Palestrante } from '@app/models/Palestrante';
 import { PalestranteService } from '@app/services/palestrante.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
@@ -29,6 +30,24 @@ export class PalestranteDetalheComponent implements OnInit {
   ngOnInit(): void {
     this.validation();
     this.verificaForm();
+    this.carregarPalestrante();
+  }
+
+  private carregarPalestrante() {
+    this.spinner.show();
+    this.palestranteService
+      .getPalestrante()
+      .subscribe(
+        (palestrante: Palestrante) => this.form.patchValue(palestrante),
+        (error) => {
+          console.error(error);
+          this.toastr.error(
+            'Não foi possível carregar os detalhes do palestrante.',
+            'Não foi possível'
+          );
+        }
+      )
+      .add(() => this.spinner.hide());
   }
 
   private verificaForm() {
@@ -42,17 +61,25 @@ export class PalestranteDetalheComponent implements OnInit {
         tap(() => this.spinner.show())
       )
       .subscribe(() => {
-        this.palestranteService.put({ ...this.form.value }).subscribe(
-          () => {
-            this.situacaoDoForm = 'Minicurrículo foi atualizado!';
-            this.corDaDescricao = 'text-success';
-          },
-          (error) => {
-            console.error(error);
-            this.toastr.error('Erro ao atualizar o minicurrículo', 'Erro');
-          }
-          ).add(() => this.spinner.hide());
-        });
+        this.palestranteService
+          .put({ ...this.form.value })
+          .subscribe(
+            () => {
+              this.situacaoDoForm = 'Minicurrículo foi atualizado!';
+              this.corDaDescricao = 'text-success';
+
+              setTimeout(() => {
+                this.situacaoDoForm = 'Minicurrículo foi atualizado!';
+                this.corDaDescricao = 'text-muted';
+              }, 2000);
+            },
+            (error) => {
+              console.error(error);
+              this.toastr.error('Erro ao atualizar o minicurrículo', 'Erro');
+            }
+          )
+          .add(() => this.spinner.hide());
+      });
   }
 
   private validation() {
